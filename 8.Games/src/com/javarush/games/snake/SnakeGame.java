40f0bf98f25537b7300a622a02bc5f960e7722d5
Aleksey Makaenko <makalex74@yandex.ru>
2020-09-10 21:07:08 +0300

    SnakeGame 19

--- 8.Games/src/com/javarush/games/snake/SnakeGame.java
+++ 8.Games/src/com/javarush/games/snake/SnakeGame.java
@@ -3,4 +3,49 @@ package com.javarush.games.snake;
 import com.javarush.engine.cell.*;
 
 public class SnakeGame extends Game {
+    public static final int WIDTH = 15;
+    public static final int HEIGHT = 15;
+    private Snake snake;
+    private Apple apple;
+    private int turnDelay;
+
+    @Override
+    public void initialize() {
+        setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+    private void createGame(){
+        turnDelay = 300;
+        setTurnTimer(turnDelay);
+        snake = new Snake(WIDTH / 2, HEIGHT / 2);
+        createNewApple();
+        drawScene();
+    }
+    private void drawScene(){
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                setCellValueEx(x, y, Color.DARKSEAGREEN,"");
+            }
+        }
+        snake.draw(this);
+        apple.draw(this);
+    }
+
+    @Override
+    public void onTurn(int step) {
+        snake.move(apple);
+        if (apple.isAlive == false) createNewApple();
+        drawScene();
+    }
+
+    @Override
+    public void onKeyPress(Key key) {
+        if (key == Key.LEFT) snake.setDirection(Direction.LEFT);
+        if (key == Key.RIGHT) snake.setDirection(Direction.RIGHT);
+        if (key == Key.UP) snake.setDirection(Direction.UP);
+        if (key == Key.DOWN) snake.setDirection(Direction.DOWN);
+    }
+    private void createNewApple(){
+        apple = new Apple(getRandomNumber(WIDTH),getRandomNumber(HEIGHT));
+    }
 }

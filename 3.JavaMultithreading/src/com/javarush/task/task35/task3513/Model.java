04d1b611b5557df103d472f31acb0a6627ef2e33
Aleksey Makaenko <makalex74@yandex.ru>
2020-11-26 18:08:43 +0300

    Level 31 2048

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,61 +1,270 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.*;
+
 public class Model {
-    private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
+    private static final int FIELD_WIDTH = 4;
+    int maxTile = 2;
+    int score = 0;
+    private boolean isSaveNeeded = true;
+    private Stack<Tile[][]> previousStates = new Stack<>();
+    private Stack<Integer> previousScores = new Stack<>();
 
     public Model() {
+        resetGameTiles();
+    }
+
+    Tile[][] getGameTiles() {
+        return gameTiles;
+    }
+
+    void resetGameTiles() {
         gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
         for (int i = 0; i < FIELD_WIDTH; i++) {
             for (int j = 0; j < FIELD_WIDTH; j++) {
                 gameTiles[i][j] = new Tile();
             }
         }
+        addTile();
+        addTile();
     }
 
     private void addTile() {
+        List<Tile> emptyTiles = getEmptyTiles();
+        if (!emptyTiles.isEmpty()) {
+            int index = (int) (Math.random() * emptyTiles.size()) % emptyTiles.size();
+            Tile emptyTile = emptyTiles.get(index);
+            emptyTile.value = Math.random() < 0.9 ? 2 : 4;
+        }
+    }
 
+    private List<Tile> getEmptyTiles() {
+        final List<Tile> list = new ArrayList<Tile>();
+        for (Tile[] tileArray : gameTiles) {
+            for (Tile t : tileArray)
+                if (t.isEmpty()) {
+                    list.add(t);
+                }
+        }
+        return list;
+    }
+
+    private boolean compressTiles(Tile[] tiles) {
+        int insertPosition = 0;
+        boolean result = false;
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            if (!tiles[i].isEmpty()) {
+                if (i != insertPosition) {
+                    tiles[insertPosition] = tiles[i];
+                    tiles[i] = new Tile();
+                    result = true;
+                }
+                insertPosition++;
+            }
+        }
+        return result;
+    }
+
+    private boolean mergeTiles(Tile[] tiles) {
+        boolean result = false;
+        LinkedList<Tile> tilesList = new LinkedList<>();
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            if (tiles[i].isEmpty()) {
+                continue;
+            }
+
+            if (i < FIELD_WIDTH - 1 && tiles[i].value == tiles[i + 1].value) {
+                int updatedValue = tiles[i].value * 2;
+                if (updatedValue > maxTile) {
+                    maxTile = updatedValue;
+                }
+                score += updatedValue;
+                tilesList.addLast(new Tile(updatedValue));
+                tiles[i + 1].value = 0;
+                result = true;
+            } else {
+                tilesList.addLast(new Tile(tiles[i].value));
+            }
+            tiles[i].value = 0;
+        }
+
+        for (int i = 0; i < tilesList.size(); i++) {
+            tiles[i] = tilesList.get(i);
+        }
+
+        return result;
+    }
+
+    private Tile[][] rotateClockwise(Tile[][] tiles) {
+        final int N = tiles.length;
+        Tile[][] result = new Tile[N][N];
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                result[c][N - 1 - r] = tiles[r][c];
+            }
+        }
+        return result;
+    }
+
+    public void left() {
+        if (isSaveNeeded) {
+            saveState(gameTiles);
+        }
+        boolean moveFlag = false;
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            if (compressTiles(gameTiles[i]) | mergeTiles(gameTiles[i])) {
+                moveFlag = true;
+            }
+        }
+        if (moveFlag) {
+            addTile();
+        }
+        isSaveNeeded = true;
+    }
+
+    public void right() {
+        saveState(gameTiles);
+        gameTiles = rotateClockwise(gameTiles);
+        gameTiles = rotateClockwise(gameTiles);
+        left();
+        gameTiles = rotateClockwise(gameTiles);
+        gameTiles = rotateClockwise(gameTiles);
+    }
+
+    public void up() {
+        saveState(gameTiles);
+        gameTiles = rotateClockwise(gameTiles);
+        gameTiles = rotateClockwise(gameTiles);
+        gameTiles = rotateClockwise(gameTiles);
+        left();
+        gameTiles = rotateClockwise(gameTiles);
+    }
+
+    public void down() {
+        saveState(gameTiles);
+        gameTiles = rotateClockwise(gameTiles);
+        left();
+        gameTiles = rotateClockwise(gameTiles);
+        gameTiles = rotateClockwise(gameTiles);
+        gameTiles = rotateClockwise(gameTiles);
+    }
+
+    private int getEmptyTilesCount() {
+        return getEmptyTiles().size();
+    }
+
+    private boolean isFull() {
+        return getEmptyTilesCount() == 0;
+    }
+
+    boolean canMove() {
+        if (!isFull()) {
+            return true;
+        }
+
+        for (int x = 0; x < FIELD_WIDTH; x++) {
+            for (int y = 0; y < FIELD_WIDTH; y++) {
+                Tile t = gameTiles[x][y];
+                if ((x < FIELD_WIDTH - 1 && t.value == gameTiles[x + 1][y].value)
+                        || ((y < FIELD_WIDTH - 1) && t.value == gameTiles[x][y + 1].value)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void saveState(Tile[][] tiles) {
+        Tile[][] tempTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                tempTiles[i][j] = new Tile(tiles[i][j].value);
+            }
+        }
+        previousStates.push(tempTiles);
+        previousScores.push(score);
+        isSaveNeeded = false;
+    }
+
+    public void rollback() {
+        if (!previousStates.isEmpty() && !previousScores.isEmpty()) {
+            gameTiles = previousStates.pop();
+            score = previousScores.pop();
+        }
+    }
+
+    void randomMove() {
+        int n = ((int) (Math.random() * 100)) % 4;
+        switch (n) {
+            case 0:
+                left();
+                break;
+            case 1:
+                up();
+                break;
+            case 2:
+                down();
+                break;
+            case 3:
+                right();
+                break;
+        }
+    }
+
+    private MoveEfficiency getMoveEfficiency(Move move) {
+        MoveEfficiency moveEfficiency = new MoveEfficiency(-1, 0, move);
+        move.move();
+        if (hasBoardChanged()) {
+            moveEfficiency = new MoveEfficiency(getEmptyTilesCount(), score, move);
+        }
+        rollback();
+        return moveEfficiency;
+    }
+
+    private boolean hasBoardChanged() {
+        for (int i = 0; i < FIELD_WIDTH; i++) {
+            for (int j = 0; j < FIELD_WIDTH; j++) {
+                if (gameTiles[i][j].value != previousStates.peek()[i][j].value) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void autoMove() {
+        PriorityQueue<MoveEfficiency> moveEfficiencies = new PriorityQueue<>(4, Collections.reverseOrder());
+
+        moveEfficiencies.offer(getMoveEfficiency(this::left));
+        moveEfficiencies.offer(getMoveEfficiency(this::up));
+        moveEfficiencies.offer(getMoveEfficiency(this::right));
+        moveEfficiencies.offer(getMoveEfficiency(this::down));
+
+        moveEfficiencies.peek().getMove().move();
     }
 }
 
 /*
-/*
-2048 (4)
-Игра 2048 начинается на поле, где две плитки уже имеют какие-то начальные значения. А наше поле пока пусто :(.
-Прежде чем бросаться писать код, давай подумаем как это можно было бы реализовать.
-Предлагаю создать приватный метод addTile, который будет смотреть какие плитки пустуют и, если такие имеются,
-менять вес одной из них, выбранной случайным образом, на 2 или 4 (на 9 двоек должна приходиться 1 четверка).
-Получить случайный объект из списка можешь использовав следующее выражение:
-(размерСписка * случайноеЧислоОтНуляДоЕдиницы).
-Также получение свободных плиток можно вынести в отдельный приватный метод getEmptyTiles, возвращающий список
-свободных плиток в массиве gameTiles.
-После реализации функционала добавления новых плиток, добавим в конструктор два вызова метода addTile,
-выполняя начальное условие задачи.
-P.S. Пожалуй стоит весь код из конструктора переместить в метод resetGameTiles, для того, чтобы при необходимости
-начать новую игру, не приходилось создавать новую модель, а можно было бы просто вернуться в начальное состояние
-вызвав его. Уровень доступа должен быть шире приватного.
-P.P.S. Для вычисления веса новой плитки используй выражение (Math.random() < 0.9 ? 2 : 4).
+2048 (16)
+Осталось совсем немного! У нас есть способ вычислить эффективность любого хода, а также мы можем их сравнивать между собой.
+
+Давай реализуем метод autoMove в классе Model, который будет выбирать лучший из возможных ходов и выполнять его.
+
+Сделаем так:
+1) Создадим локальную PriorityQueue с параметром Collections.reverseOrder() (для того, чтобы вверху очереди всегда
+был максимальный элемент) и размером равным четырем.
+2) Заполним PriorityQueue четырьмя объектами типа MoveEfficiency (по одному на каждый вариант хода).
+3) Возьмем верхний элемент и выполним ход связанный с ним.
+
+После реализации метода autoMove добавим его вызов в метод keyPressed класса Controller по нажатию на клавишу A (код - KeyEvent.VK_A).
+P.S. В качестве факультативного задания можешь почитать про указатели на методы и попробовать передать аргумент
+в метод getMoveEfficiency используя оператор "::". Для метода left должно получиться getMoveEfficiency(this::left).
+Альтернативно можешь использовать внутренний анонимный класс.
 
 Требования:
-1. Метод getEmptyTiles должен возвращать список пустых плиток в массиве gameTiles.
-2. Метод addTile должен изменять значение случайной пустой плитки в массиве gameTiles на 2 или 4 с вероятностью 0.9 и 0.1 соответственно.
-3. Метод resetGameTiles должен заполнять массив gameTiles новыми плитками и менять значение двух из них с помощью двух вызовов метода addTile.
-4. В конструкторе класса Model должен содержаться вызов метода resetGameTiles.
-
-
-2048 (3)
-Займемся реализацией класса Model. Он будет ответственен за все манипуляции производимые с игровым полем.
-
-Но чтобы как-то манипулировать игровым полем, неплохо было бы для начала его создать!
-
-Нам понадобятся:
-1. Приватная константа FIELD_WIDTH = 4, определяющая ширину игрового поля.
-2. Приватный двумерный массив gameTiles состоящий из объектов класса Tile.
-3. Конструктор без параметров инициализирующий игровое поле и заполняющий его пустыми плитками.
-
-Требования:
-1. В классе Model должно быть создано private static final поле FIELD_WIDTH со значением равным четырем.
-2. В классе Model должно быть создано private поле gameTiles типа Tile[][].
-3. Конструктор без параметров класса Model должен заполнять массив gameTiles новыми объектами типа Tile.
-4. Массив gameTiles должен иметь размерность FIELD_WIDTH x FIELD_WIDTH.
+1. В методе autoMove должен быть создан объект типа PriorityQueue с размером равным четырем.
+2. В методе autoMove в PriorityQueue должно быть добавлено 4 объекта типа MoveEfficiency с помощью метода offer (по одному на каждый вариант хода).
+3. Метод keyPressed класса Controller должен вызывать метод autoMove у модели в случае, если была нажата клавиша с кодом KeyEvent.VK_A.
+4. В методе autoMove должен быть выполнен метод move связанный с объектом MoveEfficiency полученном с помощью метода peek или poll.
  */

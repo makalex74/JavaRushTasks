a4f3f8e2f1d853afae8529ec9613ca61d5a901f4
makalex74 <makalex74@yandex.ru>
2021-11-23 10:13:20 +0300

    task3310 (Shortener6) begin

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/HashMapStorageStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/HashMapStorageStrategy.java
@@ -35,3 +35,21 @@ public class HashMapStorageStrategy implements StorageStrategy{
         return data.get(key);
     }
 }
+
+/*
+Shortener (5)
+Давай напишем наше первое хранилище (стратегию хранилища). Внутри оно будет содержать обычный HashMap. Все стратегии будем хранить в пакете strategy.
+5.1. Создай класс HashMapStorageStrategy, реализующий интерфейс StorageStrategy.
+5.2. Добавь в класс поле HashMap<Long, String> data. В нем будут храниться наши данные.
+5.3. Реализуй в классе все необходимые методы. Реализации методов должны использовать поле data. Дополнительные поля не создавать.
+
+
+Requirements:
+1. Класс HashMapStorageStrategy должен поддерживать интерфейс StorageStrategy.
+2. В классе HashMapStorageStrategy должно быть создано и инициализировано приватное поле data типа HashMap.
+3. В классе HashMapStorageStrategy должен быть корректно реализован метод containsKey.
+4. В классе HashMapStorageStrategy должен быть корректно реализован метод containsValue.
+5. В классе HashMapStorageStrategy должен быть корректно реализован метод put.
+6. В классе HashMapStorageStrategy должен быть корректно реализован метод getValue.
+7. В классе HashMapStorageStrategy должен быть корректно реализован метод getKey.
+ */

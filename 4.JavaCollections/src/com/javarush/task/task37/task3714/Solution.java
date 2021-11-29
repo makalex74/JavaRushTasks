dae8781ac2fa6348d9915c14ad734da5eafe52bc
makalex74 <makalex74@yandex.ru>
2021-11-29 17:27:58 +0300

    task3714 begin1

--- 4.JavaCollections/src/com/javarush/task/task37/task3714/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3714/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task37.task3714;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,5 +20,35 @@ public class Solution {
     }
 
     public static int romanToInteger(String s) {
+        char[] letters = s.toCharArray();
+        int[] digits = new int[letters.length];
+        for (int i=0; i< letters.length; i++) {
+            switch (letters[i]) {
+                case 'I':
+                    digits[i]=1;
+                    break;
+                case 'V':
+                    digits[i]=5;
+                    break;
+                case 'X':
+                    digits[i]=10;
+                    break;
+                case 'L':
+                    digits[i]=50;
+                    break;
+                case 'C':
+                    digits[i]=100;
+                    break;
+                case 'D':
+                    digits[i]=500;
+                    break;
+                case 'M':
+                    digits[i]=1000;
+                    break;
+            }
+        }
+        System.out.println(Arrays.toString(digits));
+        //{I, V, X, L, C, D, M}
+        return 0;
     }
 }

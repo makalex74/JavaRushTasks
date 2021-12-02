f9476583e3256510ca2c032379919dd0d437f449
makalex74 <makalex74@yandex.ru>
2021-12-02 16:45:50 +0300

    task3913 Log Parser (6) end add out into Solution

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -9,5 +9,9 @@ public class Solution {
         LogParser logParser = new LogParser(Paths.get("D:\\IdeaProjects\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task39\\task3913\\logs"));
         System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
         System.out.println(logParser.getAllUsers());
+        System.out.println(logParser.execute("get ip"));
+        System.out.println(logParser.execute("get ip for user = \"Vasya Pupkin\""));
+        System.out.println(logParser.execute("get user for event = \"DONE_TASK\""));
+        System.out.println(logParser.execute("get event for date = \"03.01.2014 03:45:23\""));
     }
 }

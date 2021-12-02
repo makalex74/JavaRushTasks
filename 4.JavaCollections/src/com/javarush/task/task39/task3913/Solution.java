6f5c52c801367117a9685a5f4d188720c590901b
makalex74 <makalex74@yandex.ru>
2021-12-02 16:49:06 +0300

    task3913 Log Parser (7) end

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -13,5 +13,6 @@ public class Solution {
         System.out.println(logParser.execute("get ip for user = \"Vasya Pupkin\""));
         System.out.println(logParser.execute("get user for event = \"DONE_TASK\""));
         System.out.println(logParser.execute("get event for date = \"03.01.2014 03:45:23\""));
+        System.out.println(logParser.execute("get ip for user = \"Eduard Petrovich Morozko\" and date between \"11.12.2013 0:00:00\" and \"03.01.2014 23:59:59\""));
     }
 }

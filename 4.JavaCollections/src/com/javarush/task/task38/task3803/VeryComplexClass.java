0884b7686246890c9c6f0c2ed704d6aeea64d55d
makalex74 <makalex74@yandex.ru>
2021-12-02 18:49:25 +0300

    task3803 (Exceptions) end

--- 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3803/VeryComplexClass.java
@@ -10,14 +10,18 @@ Runtime исключения (unchecked exception)
 
 public class VeryComplexClass {
     public void methodThrowsClassCastException() {
-
+        List<Integer> list = new ArrayList<Integer>();
+        ((LinkedList<Integer>) list).add(3);
     }
 
     public void methodThrowsNullPointerException() {
-
+        Object obj = null;
+        System.out.println(obj.getClass().getName());
     }
 
     public static void main(String[] args) {
-
+        VeryComplexClass veryComplexClass = new VeryComplexClass();
+        veryComplexClass.methodThrowsClassCastException();
+        //veryComplexClass.methodThrowsNullPointerException();
     }
 }

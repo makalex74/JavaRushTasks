f561e5823e07ca7fdb9a824e1669014e9cce80e1
makalex74 <makalex74@yandex.ru>
2021-12-02 15:32:27 +0300

    task3913 Log Parser (1) end

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -5,7 +5,8 @@ import java.util.Date;
 
 public class Solution {
     public static void main(String[] args) {
-        LogParser logParser = new LogParser(Paths.get("c:/logs/"));
+        //LogParser logParser = new LogParser(Paths.get("c:/logs/"));
+        LogParser logParser = new LogParser(Paths.get("D:\\IdeaProjects\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task39\\task3913\\logs"));
         System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
     }
 }

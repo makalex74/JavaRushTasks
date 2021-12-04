f583b5aa73f9cc615ce9a9b16ff0c0fd9a125f8d
makalex74 <makalex74@yandex.ru>
2021-12-04 13:41:32 +0300

    task3908 (Is a palindrome possible?) end

--- 4.JavaCollections/src/com/javarush/task/task39/task3908/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3908/Solution.java
@@ -10,6 +10,22 @@ public class Solution {
     }
 
     public static boolean isPalindromePermutation(String s) {
+        boolean foundOdd = false;
+        int[] tableCount = new int[256];
 
+        for (char c : s.toLowerCase().toCharArray()) {
+            tableCount[c] += 1;
+        }
+
+        for (int count : tableCount) {
+            if (count % 2 != 0) {
+                if (foundOdd) {
+                    return false;
+                }
+                foundOdd = true;
+            }
+        }
+
+        return true;
     }
 }

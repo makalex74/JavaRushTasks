9470d485bea221913a2ca702d2839ee4423f207f
makalex74 <makalex74@yandex.ru>
2021-12-04 16:19:26 +0300

    task4007 (Working with dates) end

--- 4.JavaCollections/src/com/javarush/task/task40/task4007/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4007/Solution.java
@@ -18,6 +18,41 @@ public class Solution {
     }
 
     public static void printDate(String date) {
-        //напишите тут ваш код
+        boolean printDate = true;
+        boolean printTime = true;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        if (date.contains(".") && !date.contains(":")) {
+            dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+            printTime = false;
+        }
+        if (date.contains(":") && !date.contains(".")) {
+            dateFormat = new SimpleDateFormat("HH:mm:ss");
+            printDate = false;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        try {
+            calendar.setTime(dateFormat.parse(date));
+
+            if (printDate) {
+                System.out.println("День: " + calendar.get(Calendar.DATE));
+                System.out.println("День недели: " + (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY ? 7 : calendar.get(Calendar.DAY_OF_WEEK) - 1));
+                System.out.println("День месяца: " + calendar.get(Calendar.DAY_OF_MONTH));
+                System.out.println("День года: " + calendar.get(Calendar.DAY_OF_YEAR));
+                System.out.println("Неделя месяца: " + calendar.get(Calendar.WEEK_OF_MONTH));
+                System.out.println("Неделя года: " + calendar.get(Calendar.WEEK_OF_YEAR));
+                System.out.println("Месяц: " + (calendar.get(Calendar.MONTH) + 1));
+                System.out.println("Год: " + calendar.get(Calendar.YEAR));
+            }
+            if (printTime) {
+                System.out.println("AM или PM: " + (calendar.get(Calendar.AM_PM) == Calendar.PM ? "PM" : "AM"));
+                System.out.println("Часы: " + calendar.get(Calendar.HOUR));
+                System.out.println("Часы дня: " + calendar.get(Calendar.HOUR_OF_DAY));
+                System.out.println("Минуты: " + calendar.get(Calendar.MINUTE));
+                System.out.println("Секунды: " + calendar.get(Calendar.SECOND));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }

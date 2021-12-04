dce99d544fda080f912f11935680e79a751cefb3
makalex74 <makalex74@yandex.ru>
2021-12-04 16:24:44 +0300

    task4012 (Useful DateTime API methods) end

--- 4.JavaCollections/src/com/javarush/task/task40/task4012/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4012/Solution.java
@@ -16,17 +16,22 @@ public class Solution {
     }
 
     public static boolean isLeap(LocalDate date) {
-
+        return date.isLeapYear();
     }
 
     public static boolean isBefore(LocalDateTime dateTime) {
-
+        return dateTime.isBefore(LocalDateTime.now());
     }
 
     public static LocalTime addTime(LocalTime time, int n, ChronoUnit chronoUnit) {
+        return time.plus(n, chronoUnit);
     }
 
     public static Period getPeriodBetween(LocalDate firstDate, LocalDate secondDate) {
-
+        if (firstDate.isBefore(secondDate)) {
+            return Period.between(firstDate, secondDate);
+        } else {
+            return Period.between(secondDate, firstDate);
+        }
     }
 }
